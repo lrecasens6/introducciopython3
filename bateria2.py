d5ad2277e71c11ce1e1c7f6a36da7af15d00a5c5
lrecasens6 <158268783+lrecasens6@users.noreply.github.com>
2024-02-02 12:28:59 +0100

    Update bateria2.py

--- bateria2.py
+++ bateria2.py
@@ -14,3 +14,11 @@ else:
 
 autenticar()
 
+# Exercici2:
+a = 100
+if a < 100:
+        print("El numero és menor que 100.")
+elif a ==100:
+        print("El numero és igual que 100.")
+else:
+        print("El numero és major que 100.")
