c417f5557449f8549d976cd27e7fdba4b9e601fc
lrecasens6 <158268783+lrecasens6@users.noreply.github.com>
2024-02-02 12:23:42 +0100

    Update bateria2.py

--- bateria2.py
+++ bateria2.py
@@ -1,1 +1,16 @@
+# Exercici1:
+autenticació = [
+        "nom": "Amilcar",
+        "nom": "Sila"
+]
+
+def autenticar():
+nom = input("Introdueix el nom d'usuari:")
+
+if nom == autenticació["nom"]:
+        print("Benvingut al sistema, ", nom)
+else:
+        print("Error de nom.")
+
+autenticar()
 
