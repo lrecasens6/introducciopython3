f914e5b84ce0f75812d0dae99d384a8037943e2e
lrecasens6 <158268783+lrecasens6@users.noreply.github.com>
2024-02-02 10:29:00 +0100

    Update bateria1.py

--- bateria1.py
+++ bateria1.py
@@ -1,2 +1,24 @@
+# Exercici1:
+num1 = int(input("Fica un numero:"))
+num2 = int(input("Fica un numero:"))
+resultat = num1 + num2
+print("El rsultat es", resultat)
 
+# Exercici2:
+num1 = float(input("Digues un numero:"))
+resultat = num1**2
+print("El resultat es",resultat)
 
+# Exercici3:
+nom = input("Introdueix el teu nom")
+print("Hola",nom)
+
+# Exercici4:
+es_kg = float(input("Introdueix el pes en kg:"))
+pes_lbs = pes_kg * 2,20462
+print("El pes en lliures es:",pes_lbs)
+
+# Exercici5:
+temp_c = float(input("Introdueix la temperatura en Celsius"))
+temp_f = (temp_c * 9 / 5) +32
+print("La temperatura en graus Fahrenheit es",temp_f)
