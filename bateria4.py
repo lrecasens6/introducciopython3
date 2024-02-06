4529125c314087ef70011deaa8ee7c9483695456
lrecasens6 <158268783+lrecasens6@users.noreply.github.com>
2024-02-06 10:05:59 +0100

    Update bateria4.py

--- bateria4.py
+++ bateria4.py
@@ -1,1 +1,20 @@
+# Exercici1:
+num = int(input("Fica un numero:"))
+for i in range(1,num+1):
+    if num % i == 0:
+        print(i)
 
+# Exercici2:
+resultat = 0
+suma_numeros = 0
+quantitat_numeros = 0
+while True:
+    num = int(input("Digues un numero:"))
+    if num ==0:
+        break
+    quantitat_numeros +=1
+    suma_numeros += num
+resultat = suma_numeros / quantitat_numeros
+print(resultat)
+
+# Exercici3:
