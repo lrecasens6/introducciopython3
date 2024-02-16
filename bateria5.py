53ce0548fd085c19d98c633ec85e54a123de2808
lrecasens6 <158268783+lrecasens6@users.noreply.github.com>
2024-02-16 10:27:37 +0100

    Update bateria5.py

--- bateria5.py
+++ bateria5.py
@@ -1,5 +1,15 @@
 #Exercici19
-
+resultat = 0
+llista = []
+while True:
+    valor = input("Digues un numero:")
+    if valor == "final":
+        break
+    else:
+        llista.append(int(valor))
+for numero in llista:
+    resultat = resultat + numero
+print("La suma del elements de la llista és:", resultat)
 #Exercici20
 
 #Exercici21
