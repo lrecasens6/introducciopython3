9ffe4a9d6cb26b836127db45e5f3275870f207d8
lrecasens6 <158268783+lrecasens6@users.noreply.github.com>
2024-02-19 11:45:31 +0100

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,1 +1,21 @@
+while True:
+    print("Menú principal")
+    print("Benvinguts a calc.py! Introdueix un numero de les seguents opcions:")
+    print("0. Sortir")
+    print("1. Sumar")
+    print("2. Restar")
+    print("3. Multiplicar")
+    print("4. Dividir")
+    break
 
+    opcio = int(input("Introdueix un numero de les seguents opcions:"))
+    if opcio == 0:
+        print("Chao chao chao")
+        break
+    elif opcio in [1, 2, 3, 4]:
+        num1 = float(input("Introdueix el primer numero"))
+        num2 = float(input("Introdueix el segon numero"))
+
+        if opcio == 1:
+            suma = num1 + num2
+            print("La suma del "num1" "+" "num2" es "suma" ")
