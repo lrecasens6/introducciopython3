67c2b6b12c7baa67444c246d7f9e0313451012a2
lrecasens6 <158268783+lrecasens6@users.noreply.github.com>
2024-03-12 11:39:16 +0100

    Update asteriscs.py

--- asteriscs.py
+++ asteriscs.py
@@ -1,27 +1,36 @@
-alçada = 10
-amplada = 20
-posicio_asterisc = 0
-pos_y = 0
-cadena_buida = "|"+ " " * amplada + "|"
-cadena_border = "'" * (amplada+2)
-while True:
-    cadena_asterisc = " "
-    for i range(amplada):
-        if i == posicio_asterisc
-            cadena_asterisc += "*"
-        else:
-            cadena_asterisc += " "
-    cadena_asterisc += " "
-    print(cadena_border)
-    for i in range(alçada)
-    print(' ' * posicio_asterisc + '*')
+vidas = int(input("Quantes vides tens? "))
+ample_caixa = 20
+alt_caixa = 10
+posicio_asterisc_x = 0
+posicio_asterisc_y = 0
 
-    direccio = input("Clica 'a' per moure a la esquerra, 'd' per moure a la dreta, 'w' per moure a dalt, 's' per moure a baix")
-    if direccio == 'a' and posicio_asterisc > 0:
-        posicio_asterisc -= 1
-    elif direccio == 'd' and posicio_asterisc < amplada:
-        posicio_asterisc += 1
-    if direccio == 'w' and pos_y < alçada:
-        pos_y += 1
-    elif direccio == 's' and pos_y > 0:
-        pos_y -= 1
+while True:
+    print("-" * (ample_caixa + 2))
+
+    for i in range(alt_caixa):
+        print('|', end='')
+        for i in range(ample_caixa):
+            if i == posicio_asterisc_y and j == posicio_asterisc_x:
+                print('*', end='')
+            else:
+                print(' ', end='')
+        print('|')
+
+    print("-" * (ample_caixa + 2))
+
+    tecla = input("Clica 'a' per moure a l'esquerra, 'd' per moure a la dreta, 'w' per moure cap amunt, 's' per moure cap avall i 'k' per perdre una vida: ")
+
+    if tecla == 'a' and posicio_asterisc_x > 0:
+        posicio_asterisc_x -= 1
+    elif tecla == 'd' and posicio_asterisc_x < ample_caixa - 1:
+        posicio_asterisc_x += 1
+    elif tecla == 'w' and posicio_asterisc_y > 0:
+        posicio_asterisc_y -= 1
+    elif tecla == 's' and posicio_asterisc_y < alt_caixa - 1:
+        posicio_asterisc_y += 1
+    if tecla == 'k':
+        vidas -= 1
+        print("Has perdut una vids")
+    if vidas == 0:
+        print('Game Over!')
+        break
